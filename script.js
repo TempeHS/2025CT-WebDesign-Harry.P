8581ab520029297b66458e367d8e09a6230c5e7f
PDAHarry10 <Harry.Pham969@education.nsw.gov.au>
2024-03-13 15:04:56 +1100

    Added num guess

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 let enterName = prompt("Please enter your name")
 let enterNumber;
+let computerGuess = randomNum();
+let feedback = computerGuess;
 
 
 if (enterName == "joe") {
     enterName = "joe mama"
 }
+if (enterName == "wesley") {
+    enterName = "Mike Wesley"
+}
+
 
 loadDIV();
+setTimeout(guessNumber, 1500)
+
+function randomNum() {
+return Math.floor(Math.random() * (10-1+1)+1)
+
+}
 
 function loadDIV () {
-    if (enterName == "wesley") {
-    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Mister Mike " + enterName + "<H1>"
-    }
-    else {
     document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "<H1>"
-    }
     document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number from 1 to 10</H2>"
-    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<P>Chill buddy</P>"
 }
 
 function guessNumber() {
-    let enterNumber = prompt("Enter da number")
+    let enterNumber = prompt("Enter da number");
+    addNum();
 
 }
 
+function addNum(){
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + feedback
+}
